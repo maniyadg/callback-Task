2a2c663b08461471653f934da194247db53e8734
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2022-09-08 10:58:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let container = document.createElement("div")
+container.classList.add("conatiner")
+let c = document.getElementById("span")
+c.innerHTML = 10
+document.body.append(container)
+let count = 10;
+setTimeout(()=>{
+    count = count-1
+    span.innerHTML = count
+    setTimeout(()=>{
+        count = count-1
+        span.innerHTML = count
+        setTimeout(()=>{
+            count = count-1
+            span.innerHTML = count
+            setTimeout(()=>{
+                count = count-1
+                span.innerHTML = count
+                setTimeout(()=>{
+                    count = count-1
+                    span.innerHTML = count
+                    setTimeout(()=>{
+                        count = count-1
+                        span.innerHTML = count
+                        setTimeout(()=>{
+                            count = count-1
+                            span.innerHTML = count
+                            setTimeout(()=>{
+                                count = count-1
+                                span.innerHTML = count
+                                setTimeout(()=>{
+                                    count = count-1
+                                    span.innerHTML = count
+                                    setTimeout(()=>{
+                                        count = count-1
+                                        span.innerHTML = "Happy Independence Day"
+                                        
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
